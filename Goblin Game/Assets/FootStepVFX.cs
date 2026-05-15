44a66aa3648752b9e7f2c4832b51f6fdfae1ca4d
Caden Peterson <57927858+c4den@users.noreply.github.com>
2026-05-15 01:27:20 -0400

    Developing the Color Picker

--- Goblin Game/Assets/FootStepVFX.cs
+++ Goblin Game/Assets/FootStepVFX.cs
@@ -4,6 +4,7 @@ using UnityEngine.VFX;
 public class FootStepVFX : MonoBehaviour
 {
     // This script handles footfalls and triggering smoke poof VFX when the foot hits the ground.
+    
 
     [SerializeField] private VisualEffect leftFootStep;
     [SerializeField] private VisualEffect rightFootStep;
--- Goblin Game/Assets/Scripts/NEW LOBBY/StaticPlayerPrefsHelper.cs
+++ Goblin Game/Assets/Scripts/NEW LOBBY/StaticPlayerPrefsHelper.cs
@@ -7,4 +7,9 @@ public static class StaticPlayerPrefsHelper
 
     private static string defaultUsername = "mindgob";
     public static string DefaultUsername => defaultUsername;
+
+    private static Color defaultFaceColorShift = Color.white;
+    public static Color colorShiftDefaultFaceColor => defaultFaceColorShift;
+    private static Color defaultBodyColorShift = Color.white;
+    public static Color colorShiftDefaultBodyColor => defaultBodyColorShift;
 }
