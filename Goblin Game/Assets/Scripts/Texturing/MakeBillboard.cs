0736469f3c30eb669bfa43335a0884f24323aa2c
Caden Peterson <57927858+c4den@users.noreply.github.com>
2026-06-05 00:49:21 -0400

    Changes to rail sizing

--- Goblin Game/Assets/Scripts/Texturing/MakeBillboard.cs
+++ Goblin Game/Assets/Scripts/Texturing/MakeBillboard.cs
@@ -4,6 +4,8 @@ public class MakeBillboard : MonoBehaviour
 {
     void LateUpdate()
     {
+        if (Camera.main == null)
+            return;
         transform.LookAt(Camera.main.transform);
         transform.Rotate(0f, 180f, 0f);
         
